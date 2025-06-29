03ff42f04c15fd3b61c7599bf5a60898e22a15db
engrsakib <snazmussakib01@gmail.com>
2025-06-29 10:53:46 +0600

    project setup done

--- PH client/src/App.jsx
+++ PH client/src/App.jsx
@@ -1,7 +1,6 @@
 import { FaRegBell } from 'react-icons/fa'
 import './App.css'
-import Menu from './pages/dashboard/menu/Menu'
-import dummyPhoto from '/user.svg'
+
 import { Outlet } from 'react-router'
 import { useGetLoggedInUser } from './lib/logIn/useGetLoggedInUser'
 
@@ -12,34 +11,7 @@ function App() {
 
   return (
     <>
-      {/* menu */}
-      <section className='grid grid-cols-1 md:grid-cols-12 lg:grid-cols-12 gap-4 p-4'>
-        <div className='col-span-1 md:col-span-4 lg:col-span-2 min-h-[95vh] bg-gradient-to-r from-[#7D0000] to-[#973333] p-4 rounded-lg'>
-          <Menu />
-        </div>
-
-        {/* content */}
-        <div className='col-span-1 md:col-span-8 lg:col-span-10 bg-white rounded-lg '>
-          {/* header */}
-          <header className='flex justify-between items-center mb-4 bg-[#D7B0B0] text-white p-4 rounded-lg'>
-            <h1 className='text-2xl font-bold text-[#8B0000]'>Welcome back!</h1>
-            <div className='flex items-center justify-center gap-x-5'>
-              <button className='text-[#8B0000] text-2xl bg-white rounded-full flex items-center p-4'>
-              <FaRegBell />
-            </button>
-              
-              <button className='bg-[#8B0000] text-white rounded-full flex items-center'>
-              <img src={user?.photoUrl || dummyPhoto} alt="user" className='w-[50px] object-fill h-[50px] rounded-full' />
-            </button>
-            </div>
-          </header>
-          {/* main section */}
-          <main className='overflow-y-auto p-4 '>
-            <Outlet />
-          </main>
-            
-        </div>
-      </section>
+     <h1>sakib</h1>
     </>
   )
 }
--- PH client/src/routers/RouterProvider.jsx
+++ PH client/src/routers/RouterProvider.jsx
@@ -5,21 +5,7 @@ import SignIn from "../pages/auth/signIn/SignIn";
 import ForgetFrom from "../pages/auth/forgottenPassword/ForgetFrom";
 import VerifyOTP from "../pages/auth/OTP/verifyOTP";
 import SetPassword from "../pages/auth/setNewPassword/SetPassword";
-import DashboardHome from "../pages/dashboard/home/DashboardHome";
-import AllUsers from "../pages/dashboard/users/AllUsers";
-import AllQuestions from "../pages/dashboard/questions/AllQuestions";
-import Earning from "../pages/dashboard/earning/Earning";
-import Subscription from "../pages/dashboard/subscription/Subscription";
-import Faq from "../pages/dashboard/faq/Faq";
-import Admin from "../lib/protector/Admin";
-import Public from "../lib/protector/Public";
-import PersonalInformation from "../pages/dashboard/personalProfile/PersonalInformation";
-import Terms from "../pages/dashboard/Terms & Service/Terms";
-import Editor from "../pages/dashboard/JodiEditor/Editor";
-import ReadAloud from "../pages/dashboard/questions/speking/read-aloud/ReadAloud";
-import Edit from "../pages/dashboard/questions/Edit/Edit";
-import Add from "../pages/dashboard/questions/Add/Add";
-import RepeatSentence from "../pages/dashboard/questions/speking/Repeat-Sentence/RepeatSentence";
+
 
 const router = createBrowserRouter([
   {
@@ -30,111 +16,9 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: (
-          <Admin>
-            <DashboardHome />
-          </Admin>
+          <h1>sakib</h1>
         ),
-      },
-      {
-        path: "/users",
-        element: (
-          <Admin>
-            <AllUsers />
-          </Admin>
-        ),
-      },
-      {
-        path: "/question",
-        element: (
-          <Admin>
-            <AllQuestions />
-          </Admin>
-        ),
-      },
-      {
-        path: "/question/read-aloud",
-        element: (
-          <Admin>
-            <ReadAloud />
-          </Admin>
-        ),
-      },
-      {
-        path: "/question/repeat-sentence",
-        element: (
-          <Admin>
-            <RepeatSentence />
-          </Admin>
-        ),
-      },
-      {
-        path: "/question/read-aloud/:id",
-        element: (
-          <Admin>
-            <Edit />
-          </Admin>
-        ),
-      },
-      {
-        path: "/question/read-aloud/add",
-        element: (
-          <Admin>
-            <Add />
-          </Admin>
-        ),
-      },
-      {
-        path: "/earning",
-        element: (
-          <Admin>
-            <Earning />
-          </Admin>
-        ),
-      },
-      {
-        path: "/subscription",
-        element: (
-          <Admin>
-            <Subscription />
-          </Admin>
-        ),
-      },
-      {
-        path: "/faq",
-        element: (
-          <Admin>
-            <Faq />
-          </Admin>
-        ),
-      },
-      {
-        path: "settings",
-        children: [
-          {
-            path: "profile",
-            element: (
-              <Admin>
-                <PersonalInformation />
-              </Admin>
-            ),
-          },
-          {
-            path: "terms",
-            element: (
-              <Admin>
-                <Terms />
-              </Admin>
-            ),
-          },
-          {
-            path: "terms/edit",
-            element: (
-              <Admin>
-                <Editor />
-              </Admin>
-            ),
-          },
-        ],
+      
       },
     ],
   },
@@ -177,9 +61,9 @@ const router = createBrowserRouter([
       {
         path: "recover-password/set-password",
         element: (
-          <Public>
+          
             <SetPassword />
-          </Public>
+          
         ),
       },
     ],
