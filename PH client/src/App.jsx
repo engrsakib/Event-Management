953883b2f48a0e561ed7935aa618c75ec8a75740
engrsakib <snazmussakib01@gmail.com>
2025-06-30 12:36:00 +0600

    unauthorized api call revoke

--- PH client/src/App.jsx
+++ PH client/src/App.jsx
@@ -2,13 +2,12 @@ import { FaRegBell } from 'react-icons/fa'
 import './App.css'
 
 import { Outlet } from 'react-router'
-import { useGetLoggedInUser } from './lib/logIn/useGetLoggedInUser'
+
 import Navbar from './components/public/Navbar';
 import Footer from './components/public/Footer';
 
 function App() {
-   // eslint-disable-next-line no-unused-vars
-   const { data: user, isLoading, isError, error } = useGetLoggedInUser();
+   
    
 
   return (
